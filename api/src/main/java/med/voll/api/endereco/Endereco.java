86d3ab8f3c2a444a6921e2e67812fa433f00b0b2
João Lucas <110925879+jolusca1@users.noreply.github.com>
2023-09-25 17:55:27 -0300

    criando primeira entidade

---
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,13 @@
+package med.voll.api.endereco;
+
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+}
